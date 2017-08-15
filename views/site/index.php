b93d07f41f76229a021627460c04260a6efd134d
Fernando Mauricio <fernando.mauricio@am.senac.br>
2017-08-15 15:39:12 -0400

    Versão 1.9
    Implementações
    
     Planos de Cursos
    
    - Inclusão do campo "Qnt de Alunos" no cadastro de Planos.
    
    - Inclusão do campo "Nível de Docente" no cadastro de Planos.
    
    - Realizado a inclusão do nível de docente igual a "Docente Nível Superior" via bando de dados, para todos os planos já criados, conforme acertado em reunião com DEP, GPO e DIF no mesmo dia desta publicação. Para novos planos, essa inclusão se fará normalmente no cadastro.
    
     Planilhas de Precificação
    
    - Na criação das planilhas de precificações, o sistema trará "qnt de alunos" e "nível do docente" de forma automática no momento que for escolhido o plano a ser precificado.
    
     Relatórios
    
    - Incluido na tela de relatórios DEP, o relatório em Excel que informa as planilhas de precificações criadas para as unidades, juntamente com a quantidade mínima de alunos.

--- views/site/index.php
+++ views/site/index.php
@@ -16,24 +16,23 @@ $nome_user    = $session['sess_nomeusuario'];
             </div>
             <div class="panel panel-primary">
             <div class="panel-heading">
-                        <i class="glyphicon glyphicon-star-empty"></i> O que há de novo? - Versão 1.8 - Publicado em 03/08/2017
+                        <i class="glyphicon glyphicon-star-empty"></i> O que há de novo? - Versão 1.9 - Publicado em 15/08/2017
             </div>
                     <div class="panel-body">
-                      <h4><strong style="color: #337ab7;">Implementações</strong></h4>
-                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Markup</strong></h5>
-                                <h5>- Markup passará a permitir unidades móveis com custo 0.</h5>
-                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Planilha de Curso</strong></h5>
-                                <h5>- Despesas com docentes agora podem ter custo 0.</h5>
-                                <h5>- Agora é permitido que o "Preço de Venda" seja igual a 0.</h5><br>
-                                
-                      <h4><strong style="color: #337ab7;">Correções</strong></h4>
-                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Custos da Unidade</strong></h5>
-                                <h5>- Correção de bug que não estava deletando os itens no cadastro de custos da unidade.</h5>
-                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Planilha de Curso</strong></h5>
-                                <h5>- Somente aparecerão planos ativos na listagem de planos.</h5>
-                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Relatórios</strong></h5>
-                                <h5>- Corrigido valores da coluna "Taxa de Retorno" do Relatório Geral.</h5><br>
 
+                    <div class="panel-body">
+                      <h4><strong style="color: #337ab7;">Implementações</strong></h4>
+                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Planos de Cursos</strong></h5>
+                                <h5>- Inclusão do campo "Qnt de Alunos" no cadastro de Planos.</h5>
+                                <h5>- Inclusão do campo "Nível de Docente" no cadastro de Planos.</h5>
+                                <h5>- Realizado a inclusão do nível de docente igual a "Docente Nível Superior" via bando de dados, para todos os planos já criados, conforme acertado em reunião com DEP, GPO e DIF no mesmo dia desta publicação. Para novos planos, essa inclusão se fará normalmente no cadastro.</h5><br>
+
+                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Planilhas de Precificação</strong></h5>
+                                <h5>- Na criação das planilhas de precificações, o sistema trará "qnt de alunos" e "nível do docente" de forma automática no momento que for escolhido o plano a ser precificado.</h5><br>
+
+                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Relatórios</strong></h5>
+                                <h5>- Incluido na tela de relatórios DEP, o relatório em Excel que informa as planilhas de precificações criadas para as unidades, juntamente com a quantidade mínima de alunos.</h5>
+                    </div>
                                 <p><a href="index.php?r=site/versao" class="btn btn-warning" role="button">Histórico de Versões</a></p>
                     </div>
             </div>
--- views/site/versao.php
+++ views/site/versao.php
@@ -17,7 +17,26 @@ $this->title = 'Auxílio ao Planejamento';
 
                 <div class="panel panel-primary">
                 <div class="panel-heading">
-                            <i class="glyphicon glyphicon-star-empty"></i> Versão 1.8 - (ATUALMENTE) - Publicado em 03/08/2017
+                            <i class="glyphicon glyphicon-star-empty"></i> Versão 1.9 - (ATUALMENTE) - Publicado em 15/08/2017
+                </div>
+                    <div class="panel-body">
+                      <h4><strong style="color: #337ab7;">Implementações</strong></h4>
+                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Planos de Cursos</strong></h5>
+                                <h5>- Inclusão do campo "Qnt de Alunos" no cadastro de Planos.</h5>
+                                <h5>- Inclusão do campo "Nível de Docente" no cadastro de Planos.</h5>
+                                <h5>- Realizado a inclusão do nível de docente igual a "Docente Nível Superior" via bando de dados, para todos os planos já criados, conforme acertado em reunião com DEP, GPO e DIF no mesmo dia desta publicação. Para novos planos, essa inclusão se fará normalmente no cadastro.</h5><br>
+
+                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Planilhas de Precificação</strong></h5>
+                                <h5>- Na criação das planilhas de precificações, o sistema trará "qnt de alunos" e "nível do docente" de forma automática no momento que for escolhido o plano a ser precificado.</h5><br>
+
+                        <h5><i class="glyphicon glyphicon-tag"></i><strong> Relatórios</strong></h5>
+                                <h5>- Incluido na tela de relatórios DEP, o relatório em Excel que informa as planilhas de precificações criadas para as unidades, juntamente com a quantidade mínima de alunos.</h5>
+                    </div>
+                </div>
+
+                <div class="panel panel-danger">
+                <div class="panel-heading">
+                            <i class="glyphicon glyphicon-folder-close"></i> Versão 1.8 - Publicado em 03/08/2017
                 </div>
                     <div class="panel-body">
                       <h4><strong style="color: #337ab7;">Implementações</strong></h4>
